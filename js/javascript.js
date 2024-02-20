6aabc79a0a58bc3cabb91e82692135994355d185
diyon <d.romijn@sintlucasedu.nl>
2024-02-20 10:50:05 +0100

    ricardo les 3
    added: tilemap
    assets
    player img
    
    fixed:
    map not in right place
    inventory now works
    key works
    list works
    tree opacity works

--- js/javascript.js
+++ js/javascript.js
@@ -3,38 +3,71 @@ document.getElementById("mainTitle").innerText = "Point And Click Adventure Game
 // gameWindow reference
 const gameWindow = document.getElementById("gameWindow");
 
-//FOREGROUND
-const door1 = document.getElementById("door1")
 
+let inventory = [];
+const inventoryList = document.getElementById("inventoryList");
+//FOREGROUND
+
+const door1 = document.getElementById("door1")
+const KeyElement = document.getElementById("key");
 //main character code
 const mainCharacter = document.getElementById("hero");
 const offsetCharacter = 16;
+
+
 
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
 
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+
+
+
+
+    if (e.target.id !== "heroImage") {
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
+    }
 
     console.log(e.target.id);
 
     switch (e.target.id) {
-        case "door1":
-            MainCharacter.style.backgroundColor = "#ffff00"
-            door1.style.opacity = 0.7;
+        case "door1": door1.style.opacity = 0.7;
+            break;
+
 
         case "key":
-            console.log("you found a key!");
+
+            showItem("rusty key", "rustyKey")
 
             break;
 
-        default:
-            MainCharacter.style.backgroundColor = "#ffff00"
-            door1.style.opacity = 1;
+        default: door1.style.opacity = 1;
 
             break;
+    }
+
+    function getItem(itemName, itemId) {
+
+    }
+    function checkItem(itemId) {
+
+    }
+    /**
+     * needs a name for displaying item and a html id name
+     * @param {string} itemId 
+     * @param {string} itemName 
+     */
+    function showItem(itemName, itemId) {
+        console.log("you found a " + itemName + "!");
+        const KeyElement = document.createElement("li");
+        KeyElement.id = itemId;
+        KeyElement.innerText = itemName;
+        inventoryList.appendChild(KeyElement);
+    }
+    function removeItem(itemId, itemName) {
+
     }
 
 }
