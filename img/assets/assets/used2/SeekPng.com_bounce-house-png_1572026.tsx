90d950104d0fa96fc9107c5aa1dc99135000990e
diyon <d.romijn@sintlucasedu.nl>
2024-02-29 12:06:45 +0100

    added new assets
    added new assets and deleted old ones also created the first map

---
+++ img/assets/assets/used2/SeekPng.com_bounce-house-png_1572026.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="SeekPng.com_bounce-house-png_1572026" tilewidth="32" tileheight="32" tilecount="165" columns="15">
+ <image source="../SeekPng.com_bounce-house-png_1572026.png" width="481" height="373"/>
+</tileset>
